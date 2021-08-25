bc760f1fd9f6db082a886e8839b94da266069097
Georgkali <georgijs.kalicava@gmail.com>
2021-08-25 11:03:53 +0300

    RSPv2

--- RockScissorsPaper/RSP.php
+++ RockScissorsPaper/RSP.php
@@ -12,16 +12,17 @@ if($choice < 1 || $choice > 3) {
     $choice = (int) readline("Choose your option: \n 1 -> rock \n 2 -> scissors \n 3 -> paper \n");
 }
 $playerChoice = $options[$choice - 1];
-$computerChoice = $options[rand(0, 2)];
-echo "Player: " . $playerChoice . "\n";
-echo "Computer: " . $computerChoice . "\n";
+$rand = rand(0, 2);
+$computerChoice = $options[$rand];
+echo "Player: " . ($choice) . " -> " . $playerChoice . "\n";
+echo "Computer: " . ($rand + 1) . " -> " . $computerChoice . "\n";
 
 if($playerChoice == $computerChoice) echo "Tie!";
 
-if($playerChoice == $options[0] && $computerChoice == $options[1]) echo "Player win!";
-if($playerChoice == $options[1] && $computerChoice == $options[2]) echo "Player win!";
-if($playerChoice == $options[2] && $computerChoice == $options[0]) echo "Player win!";
+if($playerChoice == $options[0] && $computerChoice == $options[1]) echo "Player wins!";
+if($playerChoice == $options[1] && $computerChoice == $options[2]) echo "Player wins!";
+if($playerChoice == $options[2] && $computerChoice == $options[0]) echo "Player wins!";
 
-if($playerChoice == $options[0] && $computerChoice == $options[2]) echo "Computer win!";
-if($playerChoice == $options[1] && $computerChoice == $options[0]) echo "Computer win!";
-if($playerChoice == $options[2] && $computerChoice == $options[1]) echo "Computer win!";
+if($playerChoice == $options[0] && $computerChoice == $options[2]) echo "Computer wins!";
+if($playerChoice == $options[1] && $computerChoice == $options[0]) echo "Computer wins!";
+if($playerChoice == $options[2] && $computerChoice == $options[1]) echo "Computer wins!";
