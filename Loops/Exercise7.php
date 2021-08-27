452e45949a06b8581154c2f98bbe2b5a07a088ff
Georgkali <georgijs.kalicava@gmail.com>
2021-08-27 23:47:27 +0300

    Loops/Exercise7.php

--- Loops/Exercise7.php
+++ Loops/Exercise7.php
@@ -4,3 +4,27 @@
 // then repeatedly rolls two six-sided dice (using a Random object to generate random numbers from 1-6)
 // until the sum of the two dice values is the desired sum.
 // Here is the expected dialogue with the user:
+
+class RollTwoDice {
+
+
+    public $result = 0;
+    public function roll() {
+
+                $input = (int)readline("Enter a number (2 - 12): ");
+        if ($input < 2 || $input > 12) {
+            exit();
+        }
+        while ($this->result !== $input) {
+
+            $x = rand(1,6);
+            $y = rand(1,6);
+            $this->result = $x + $y;
+            echo $x . " and " . $y . " = " . $this->result . PHP_EOL;
+        }
+    }
+
+}
+
+$rollTwoDice = new RollTwoDice;
+echo $rollTwoDice->roll();
