a9e700685da46305b5b04b1e1cb1b0e1f63277e8
Georgkali <georgijs.kalicava@gmail.com>
2021-09-06 09:22:35 +0300

    Olympic Games

--- OlimpicGames/OlimpicGames.php
+++ OlimpicGames/OlimpicGames.php
@@ -1,63 +1,67 @@
 <?php
 
-$line = ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"];
-$finish = [];
-
-function makePlayer($name, $start):stdClass {
-    $player = new stdClass();
-    $player->name = $name;
-    $player->start = $start;
-    return $player;
-}
-
-$players = [
-    makePlayer("#", 0),
-    makePlayer("@", 0),
-    makePlayer("%", 0)
-];
-/*
-$run = function ($player) use ($line, &$players)
+function makeAthlete(string $name, int $position):stdClass
 {
-    $line[$player->start] = $player->name;
-    $player->start += rand(1, 2);
-    echo $player->start . implode($line) . "\n";
+    $athlete = new stdClass();
+    $athlete->name = $name;
+    $athlete->position = $position;
+    return $athlete;
+}
+function makeLine(string $pattern, int $length): array {
+    return array_fill(0,$length, $pattern);
+}
+$athletes = [
+    makeAthlete("%",0),
+    makeAthlete("$",0),
+    makeAthlete("#",0)
+];
 
-};
+$again = 'y';
+while ($again == "y") {
 
-foreach ($players as $item=>$value) {
-    for($i = $value->start; $value->start < 20;) {
-       $run($value);
-        sleep(1);
+    foreach ($athletes as $athlete) {
+        $athlete->position = 0;
+    }
+    foreach ($athletes as $key => $athlete) {
+        echo $key + 1 . "-> horse name: $athlete->name \n";
+    }
+    $lines = [
+        makeLine("-", 100),
+        makeLine("-", 100),
+        makeLine("-", 100),
+    ];
+    $results = [];
+    $run = function () use ($athletes, $lines, &$results) {
+        foreach ($athletes as $key => $athlete) {
+
+            $lines[$key][$athlete->position] = $athlete->name;
+            if ($athlete->position == count($lines[$key]) - 2) {
+                $athlete->position += 1;
+            } elseif ($athlete->position == count($lines[$key]) - 1) {
+                $results[] = $athlete->name;
+                $athlete->position += 2;
+            } else {
+                $athlete->position += rand(1, 3);
+            }
+            echo implode($lines[$key]) . "\n";
+
+        }
+        usleep(300000);
+    };
+
+    while (count($results) < 3) {
+        $run();
+        echo "\n";
+    }
+    foreach ($results as $key => $result) {
+        echo ($key + 1) . " place - $result \n";
+    }
+    $again = readline("Play again?(y/n): ");
+    if($again == "n") {
+
+        exit;
     }
 }
-*/
-
-
-$run = function ($player) use ($line, &$finish, &$players)
-{
-        $line[$player->start] = $player->name;
-        $player->start += rand(1,2);
-        echo $player->start . implode($line) . "\n";
-};
-
-   for($i = 0; $i < 20; $i++) {
-       foreach ($players as $player)
-           if($player->start < 21) {
-       $run($player);
-           } else {
-               $finish[] = $player->name;
-           }
-       sleep(1);
-       echo "\n";
-   }
-
-
-   $finish = array_unique($finish);
-   var_dump($finish);
-foreach ($finish as $key=>$value) {
-    echo  $key + 1 . "st place->  $value  \n";
-}
-
 
 
 
